d55eaa77730a9da2a3c95869f90dc6a28e4edbd2
hebersonaguiar <hebersonaguiar@gmail.com>
2019-03-31 19:21:02 +0000

    Finalizado Fase 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix'=>'admin', 'middleware'=>'auth.checkrole', 'as'=>'admin.'], function() {
+Route::group(['prefix'=>'admin', 'middleware'=>'auth.checkrole:admin', 'as'=>'admin.'], function() {
 	Route::get('categories',['as'=>'categories.index' ,'uses'=>'CategoriesController@index']);
 	Route::get('categories/create',['as'=>'categories.create','uses'=>'CategoriesController@create']);
 	Route::get('categories/edit/{id}',['as'=>'categories.edit','uses'=>'CategoriesController@edit']);
@@ -48,7 +48,7 @@ Route::group(['prefix'=>'admin', 'middleware'=>'auth.checkrole', 'as'=>'admin.']
 });
 
 
-Route::group(['prefix'=>'customer', 'as'=>'customer.'], function() {
+Route::group(['prefix'=>'customer', 'middleware'=>'auth.checkrole:client','as'=>'customer.'], function() {
 	Route::get('order',['as'=>'order.index' ,'uses'=>'CheckoutController@index']);
 	Route::get('order/create',['as'=>'order.create' ,'uses'=>'CheckoutController@create']);
 	Route::post('order/store',['as'=>'order.store' ,'uses'=>'CheckoutController@store']);
